4a059307a66dfe8ebcb2c6e995b41c56609bdf54
h4r1 <hariapi@yahoo.com>
2018-06-22 15:56:50 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ function newGame () {
 	var toReturn = {
 		global: {
 		},
-		job: [ "Farmer", "Hunter", "Woodcutter", "Iron Miner", "Stone Miner", "Breeder" 
-		      ,"Carpenter"
+		job: [ "Farmer", "Hunter", "Woodcutter", "Iron Miner", "Stone Miner", "Beggar"
+				, "Carpenter", "Innkeeper"
 		],
 	};
 	
